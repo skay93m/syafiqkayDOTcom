068ea653918d8b5c24b1316951c2cfeb97d648ef
Syafiq <syafiq@syafiqkay.com>
2025-07-10 20:04:57 +0000

    Add a blank line after the module docstring in settings.py

--- sk/settings.py
+++ sk/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+
 # Path settings
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
