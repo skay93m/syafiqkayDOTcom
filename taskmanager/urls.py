92fac2605bf5d6a7a55b20378e33bc128d12610f
Syafiq <syafiq@syafiqkay.com>
2025-07-16 17:07:02 +0000

    feat: Refactor task manager views and URLs, enhance templates, and implement CRUD functionality

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 app_name = 'taskmanager'
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('help/', views.help, name='help'),
-    path('tasks/', views.task_list, name='task_list'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('help/', views.HelpView.as_view(), name='help'),
+    path('tasks/', views.TaskListView.as_view(), name='task-list'),
+    path('tasks/<int:pk>/', views.TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/create/', views.TaskCreateView.as_view(), name='task-create'),
+    path('tasks/<int:pk>/update/', views.TaskUpdateView.as_view(), name='task-update'),
+    path('tasks/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task-delete'),
 ]
--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 
-def home(request):
-    return render(request, 'taskmanager/taskmanager.html')
+class TaskListView(ListView):
+    model = Task
+    template_name = 'taskmanager/task_list.html'
+    context_object_name = 'tasks'
 
-def help(request):
-    return render(request, 'taskmanager/help.html')
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'taskmanager/task_detail.html'
+    context_object_name = 'task'
 
-def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, 'taskmanager/task_list.html', {'tasks': tasks})
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = 'taskmanager/task_form.html'
+    fields = ['title', 'description', 'status', 'due_date', 'owner', 'epic',]
+    
+    def get_success_url(self):
+        return reverse_lazy('task-detail', kwargs={'pk': self.object.id})
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'taskmanager/task_form.html'
+    fields = ['title', 'description', 'status', 'due_date', 'owner', 'epic']
+
+    def get_success_url(self):
+        return reverse_lazy('task-detail', kwargs={'pk': self.object.id})
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'taskmanager/task_confirm_delete.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('task-list')
+
+class HomeView(TemplateView):
+    template_name = 'taskmanager/taskmanager.html'
+
+class HelpView(TemplateView):
+    template_name = 'taskmanager/help.html'
